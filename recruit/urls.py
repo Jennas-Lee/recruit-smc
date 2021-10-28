54070c3d4d177842eb4316e1f3c775365c5eb3d2
Jennas-Lee <leezeunjun@gmail.com>
2021-10-28 13:03:52 +0900

    Add recruit info form

--- recruit/urls.py
+++ recruit/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from recruit.views import index
+from recruit.views import index, form_info
 
 urlpatterns = [
     path('', index, name='recruit-index'),
+    path('info/', form_info, name='recruit-form-info')
 ]
--- recruit/views.py
+++ recruit/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+start_timestamp = 1637625600
+end_timestamp = 1637740800
+now_timestamp = timezone.now().timestamp()
+
+if start_timestamp < now_timestamp < end_timestamp:
+    recruit_available = True
+else:
+    recruit_available = False
+
+# TODO: Update recruit_available
+recruit_available = True
 
 
 def index(request):
-    return render(request, 'recruit.html')
+    return render(request, 'recruit_index.html', {'recruit_available': recruit_available})
+
+
+def form_info(request):
+    if request.method == 'GET':
+        return render(request, 'recruit_form_info.html', {'recruit_available': recruit_available})
