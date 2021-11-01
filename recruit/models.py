3069bd64c0df13975717306931731ffbdcbcd642
Jennas-Lee <leezeunjun@gmail.com>
2021-11-01 10:08:28 +0900

    Add additional input form in recruit info form

--- recruit/models.py
+++ recruit/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(null=False, max_length=5)
-    school = models.CharField(null=False, max_length=21)
+    school = models.CharField(null=False, max_length=30)
     grade = models.CharField(null=True, blank=True, max_length=1)
     _class = models.CharField(null=True, blank=True, max_length=2)
     number = models.CharField(null=True, blank=True, max_length=2)
