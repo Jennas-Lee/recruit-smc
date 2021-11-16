942cc7bf13f7b81ab402b51af9fd4a8d60c7ac70
Jennas-Lee <leezeunjun@gmail.com>
2021-11-16 10:38:52 +0900

    Release 0.2-hotfix7

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 bash /etc/profile.d/codedeploy.sh
-docker login -u AWS -p $(aws ecr get-login-password --region $AWS_DEFAULT_REGION) $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
